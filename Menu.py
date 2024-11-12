c70542d2b52d8bf536e37921fb6be4810d15ff30
MrHH-k22 <nguyenmaihuyhoang156@gmail.com>
2024-11-12 13:46:08 +0700

    create hoang branch

--- Menu.py
+++ Menu.py
@@ -277,9 +277,91 @@ class twoPlayerMenu(Menu):
         self.cursorGA = WHITE
         self.cursorHuman = WHITE
 
-        self.BFSx, self.BFSy = self.mid_size, self.mid_size - 50
-        self.DFSx, self.DFSy = self.mid_size, self.mid_size + 0
-        self.ASTARx, self.ASTARy = self.mid_size, self.mid_size + 50
+        # For Player 1
+        self.title1x, self.title1y = self.mid_size-150, self.mid_size - 50
+        self.BFSx, self.BFSy = self.mid_size-150, self.mid_size + 0
+        self.DFSx, self.DFSy = self.mid_size-150, self.mid_size + 50
+        self.ASTARx, self.ASTARy = self.mid_size-150, self.mid_size + 100
+        self.GAx, self.GAy = self.mid_size-150, self.mid_size + 150
+        self.Humanx, self.Humany = self.mid_size-150, self.mid_size + 200
+        # For player 2
+        self.title2x, self.title2y = self.mid_size + 150, self.mid_size - 50
+        
+
+        self.cursor_rect.midtop = (self.BFSx + self.offset, self.BFSy)
+        
+    def change_cursor_color(self):
+        self.clear_cursor_color()
+        if self.state == 'BFS':
+            self.cursorBFS = MENU_COLOR
+        elif self.state == 'DFS':
+            self.cursorDFS = MENU_COLOR
+        elif self.state == 'ASTAR':
+            self.cursorASTAR = MENU_COLOR
+        elif self.state == 'GA':
+            self.cursorGA = MENU_COLOR
+        elif self.state == 'HUMAN':
+            self.cursorHuman = MENU_COLOR
+
+    def clear_cursor_color(self):
+        self.cursorBFS = WHITE
+        self.cursorDFS = WHITE
+        self.cursorASTAR = WHITE
+        self.cursorGA = WHITE
+        self.cursorHuman = WHITE
+
+        
+    def display_menu(self):
+        self.run_display = True
+
+        while self.run_display:
+            self.game.event_handler()
+            self.check_input()
+
+            self.game.display.fill(WINDOW_COLOR)
+
+            self.game.draw_text(
+                'Ai Snake Game', size=self.title_size,
+                x=self.game.SIZE/2, y=self.game.SIZE/2 - 2*(CELL_SIZE + NO_OF_CELLS),
+                color=TITLE_COLOR
+            )
+            # create button for player 1
+            self.game.draw_text(
+                'Player 1', size=self.option_size,
+                x=self.title1x, y=self.title1y,
+                color= WHITE
+            )
+
+            self.game.draw_text(
+                'BFS', size=self.option_size,
+                x=self.BFSx,  y=self.BFSy,
+                color=self.cursorBFS
+            )
+            
+            # create button for player 2
+            self.game.draw_text(
+                'Player 2', size=self.option_size,
+                x=self.title2x, y=self.title2y,
+                color= WHITE
+            )
+
+
+            self.draw_cursor()
+            self.change_cursor_color()
+            self.blit_menu()
+        
+    def check_input(self):
+        self.move_cursor()
+
+        if self.game.START:
+            if self.state == 'GA':  # go to genetic algorith options
+                self.game.curr_menu = self.game.GA
+            else:
+                self.game.playing = True
+            self.run_display = False
+
+    def move_cursor(self):
+        pass
         
         
 
